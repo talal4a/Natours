4430f5de175fedaeeeb2a03be39bd5828ca3438e
talal4a <talalxus000@gmail.com>
2025-06-28 16:10:02 +0500

    update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const morgan = require("morgan");
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
   console.log("Hello from the middleware");
   next();
--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 const app = require("./app");
+console.log("PORT:", process.env.PORT);
+console.log("USER:", process.env.USER);
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
